76e0deb02a6fbd1e5a9a04f78351511753b05d2d
kavindu-98 <kavinduumayanga058@gmail.com>
2024-07-02 22:49:46 +0530

    add commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -97,11 +97,13 @@ export const Projects = () => {
                 >
                   <h2>Projects</h2>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
+                    My portfolio includes diverse projects: a transport
+                    management app, a travel booking app, and a student quiz
+                    app, all showcasing my design and development skills. Web
+                    projects feature transport management, gym management, and
+                    children home management applications. Additionally, my
+                    graphic design work includes freelance flyer and digital
+                    marketing flyer designs.
                   </p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
